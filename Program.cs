d73126f4444c3211e2b2bc56949df7e16362f914
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2023-01-02 16:14:31 +0300

    Первые шаги

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Ввод строк с клавиатуры
+      // и образование нового массива
